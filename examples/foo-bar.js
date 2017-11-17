3211947555e44c18654c6809e1c9fb87938aca26
Jesse <jesse@piascik.net>
2017-11-17 16:00:31 -0700

    Update project

--- examples/foo-bar.js
+++ examples/foo-bar.js
@@ -2,15 +2,16 @@ var sweetRepl = require('../index'); // You should use require('sweet-repl');
 
 sweetRepl
   .command('foo', 'prints foo:<str>'
-           ,function(str) { 
+           ,function(str) {
              this.keyVal('foo',str);
              return str;
            })
-  .command('bar', 'prints bar:<str>' 
-           ,function(str) { 
+  .command('bar', 'prints bar:<str>'
+           ,function(str) {
              this.keyVal('bar',str);
              return str;
            })
   .start({ prompt: 'sweet-repl>' }, 20000);
-
+// TODO: remove this coment gh:2
+// TODO: Add a new task
 // now `telnet localhost 20000` in terminal and execute `help()`
--- index.js
+++ index.js
@@ -21,17 +21,17 @@ var net  = require('net')
  *
  * sweetRepl
  *   .command('foo', 'prints foo:<str>'
- *            ,function(str) { 
+ *            ,function(str) {
  *              this.keyVal('foo',str);
  *              return str;
  *            })
- *   .command('bar', 'prints bar:<str>' 
- *            ,function(str) { 
+ *   .command('bar', 'prints bar:<str>'
+ *            ,function(str) {
  *              this.keyVal('bar',str);
  *              return str;
  *            })
  *   .start({ prompt: 'sweet-repl>' }, 20000);
- * 
+ *
  * In the terminal:
  *
  *    $ telnet localhost 20000
@@ -48,21 +48,22 @@ function SweetRepl() {
     desc: 'Display help information',
     fn: function() {
       _this = this;
-      
+
       this.title('HELP');
       this.println('  ----');
       this.println('');
 
       self.commands.forEach(function(cmd) {
         var params = cmd.fn.toString().match(/^function +\((.*?)\)/)[1];
-
         _this.keyVal(cmd.name + '(' + params + ')', cmd.desc);
       });
+      this.keyVal('.exit', 'Leave the repl')
 
       this.println();
-      this.println('----------------------------------------------------');
-      
-      return         "Thanks for using sweet-repl by <jesse@piascik.net>";
+      if (self.opts.onHelp) self.opts.onHelp.apply(this)
+      this.println();
+
+      return "+++++++++++++++++++++++++++++++++++++++++++"
     }
   }];
 }
@@ -70,23 +71,23 @@ function SweetRepl() {
 SweetRepl.prototype.start = function() {
   var args = arguments;
   if (!args.length) throw new Error('start() requires repl options and port/host or path');
-  
+
   var self = this;
   extend(this.opts, args[0]);
   // start repl
   // tcp or unix-domain socket repl
   this.server = net.createServer(function(sock){
     extend(self.opts, { input: sock, output: sock });
-    
+
     self.repl = repl.start(self.opts);
-    
+
     // destroy the socket on exit
     self.repl.on('exit', function() {
       sock.destroy();
     });
-  
+
     // tools to pass as this in command function
-    var tools = { 
+    var tools = {
       title : function(str) { sock.write('\n  \033[36m' + str + '\033[0m\n'); },
       keyVal : function(key, val) { sock.write('  \033[90m' + key + ':\033[0m ' + val + '\n'); },
       println : function(str) { sock.write((str || '') + '\n'); }
